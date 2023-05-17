43f47e5d59cd95ff29626333eb0c672d8cfe2b80
CNBigOrange <846547592@qq.com>
2023-05-18 00:01:17 +0800

    1

---
+++ Source/Material_collections/Material_collections.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Material_collections.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Material_collections, "Material_collections" );
---
+++ Source/Material_collections/Material_collectionsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Material_collectionsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MATERIAL_COLLECTIONS_API AMaterial_collectionsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
